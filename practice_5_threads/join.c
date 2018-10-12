6a65f1bebc14a6752e1b32281d37e51b09ce5651
Allar <allar.viinamae@hotmail.com>
2018-10-12 14:11:12 +0300

    threads: ex.4 done

--- practice_5_threads/join.c
+++ practice_5_threads/join.c
@@ -8,24 +8,37 @@ pthread_mutex_t counterMutex;
 
 long counter = 0; // Shared variable
 
+struct thread_data {
+    long *counter;
+    char **args;
+};
+
+struct thread_data thread_data_array[NUM_THREADS];
+
 void *threadWork(void *input) {
-    long *counter = (long *) input;
+    struct thread_data *threadData = (struct thread_data *) input;
     printf("<Thread> Starting...\n");
-    printf(" <Thread> input: %li \n", *counter);
+    printf(" <Thread> input: %li \n", *threadData->counter);
+
+    printf(" <Thread> Hello, : %s %s \n", threadData->args[1], threadData->args[2]);
 
     pthread_mutex_lock(&counterMutex);
     for (long i = 0; i < 1000000; i++) {
-        (*counter)++;
+        (*threadData->counter)++;
     }
     pthread_mutex_unlock(&counterMutex);
 
-    printf(" <Thread> output: %li \n", *counter);
+    printf(" <Thread> output: %li \n", *threadData->counter);
 
     printf("<Thread> done.\n");
     pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { // an array of char pointers
+    for (int i = 1; i < argc; i++) {
+        printf("<Main> Args: %s\n", argv[i]);
+    }
+
     pthread_t thread[NUM_THREADS];
 
     pthread_attr_t attr;
@@ -39,10 +52,15 @@ int main(int argc, char *argv[]) {
 
     pthread_mutex_init(&counterMutex, NULL);
 
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_data_array->counter = &counter;
+        thread_data_array->args = argv;
+    }
+
     int returnCode;
     for (int i = 0; i < NUM_THREADS; i++) {
-        printf("<Main> Creating thread %d\n", i);
-        returnCode = pthread_create(&thread[i], &attr, threadWork, (void *) &counter);
+        printf("<Main> Creating thread: %d\n", i);
+        returnCode = pthread_create(&thread[i], &attr, threadWork, (void *) &thread_data_array);
 
         if (returnCode) {
             printf("ERROR; return code from pthread_create() is %d\n", returnCode);
@@ -62,8 +80,8 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("Main: counter value %li \n", counter);
-    printf("Main: program completed. Exiting.\n");
+    printf("<Main> Counter value %li \n", counter);
+    printf("<Main> Program completed. Exiting.\n");
 
     pthread_mutex_destroy(&counterMutex);
     pthread_exit(NULL);
